49886cc858fd396888ad17b916a9938059414060
unknown <vikogutu77@gmail.com>
2022-07-12 07:57:08 +0300

    otp_layout

---
+++ app/src/main/java/com/mhealth/nishauri/Activities/Auth/OtpCodeActivity.java
@@ -0,0 +1,16 @@
+package com.mhealth.nishauri.Activities.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mhealth.nishauri.R;
+
+public class OtpCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_code);
+    }
+}
