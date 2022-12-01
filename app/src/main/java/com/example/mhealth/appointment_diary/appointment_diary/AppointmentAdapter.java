036e4551a1ddbecd7d7faadb8872c9b49c7bd519
unknown <vikogutu77@gmail.com>
2022-12-01 17:41:39 +0300

    Appointment calender

--- app/src/main/java/com/example/mhealth/appointment_diary/appointment_diary/AppointmentAdapter.java
+++ app/src/main/java/com/example/mhealth/appointment_diary/appointment_diary/AppointmentAdapter.java
@@ -57,7 +57,6 @@ public class AppointmentAdapter extends BaseAdapter implements Filterable {
     private Context mycont;
     private List<AppointmentModel> mylist;
     CustomFilter filter;
-
     private List<AppointmentModel> filterList;
     List<Appointments> books = null;
 
