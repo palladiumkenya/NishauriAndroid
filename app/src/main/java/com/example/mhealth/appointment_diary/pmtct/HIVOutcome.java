3e66d8a497d3a8452d6a16e5d2f591da6322d3fc
vicky-ogutu <vikogutu77@gmail.com>
2022-12-14 10:39:21 +0300

    outcome dropdowns

--- app/src/main/java/com/example/mhealth/appointment_diary/pmtct/HIVOutcome.java
+++ app/src/main/java/com/example/mhealth/appointment_diary/pmtct/HIVOutcome.java
@@ -3,15 +3,66 @@ package com.example.mhealth.appointment_diary.pmtct;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.mhealth.appointment_diary.R;
 
 public class HIVOutcome extends AppCompatActivity {
+    String[] clientIs = {"", "Breastfeeding", "Not Breastfeeding", "Pregnant", "Pregnant and Breastfeeding"};
+    String[] hivResults = {"", "Unknown", "Negative", "Positive"};
+    Spinner clientIsS, hivResultsS;
+    private String CLIENT_IS = "";
+    private String HIV_RESULTS = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hivoutcome);
+
+        clientIsS = (Spinner) findViewById(R.id.ClientSpinner);
+        hivResultsS =(Spinner) findViewById(R.id.hivResults);
+
+//      Client is
+        ArrayAdapter<String> clientAdapter = new ArrayAdapter<String>(HIVOutcome.this, android.R.layout.simple_spinner_item, clientIs);
+        clientAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        clientIsS.setAdapter(clientAdapter);
+
+        clientIsS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+             CLIENT_IS = clientIs[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+// hiv results
+        ArrayAdapter<String> resultsAdapter = new ArrayAdapter<String>(HIVOutcome.this, android.R.layout.simple_spinner_item, hivResults);
+        clientAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hivResultsS.setAdapter(resultsAdapter);
+
+        hivResultsS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                HIV_RESULTS = clientIs[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
 
         try{
             //getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -21,7 +72,8 @@ public class HIVOutcome extends AppCompatActivity {
         }
         catch(Exception e){
 
-
         }
+
+
     }
 }
