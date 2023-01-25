5f9c2a3e2738843148069222f42e7cce0338a581
vicky-ogutu <vikogutu77@gmail.com>
2023-01-25 07:40:26 +0300

    PNCVisitStart

---
+++ app/src/main/java/com/example/mhealth/appointment_diary/pmtct/PNCVisitStart.java
@@ -0,0 +1,16 @@
+package com.example.mhealth.appointment_diary.pmtct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mhealth.appointment_diary.R;
+
+public class PNCVisitStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pncvisit_start);
+    }
+}
