b01ca434e4a2ae673fdc4236fb91a2b8fe31bb46
vicky-ogutu <vikogutu77@gmail.com>
2023-03-09 09:08:13 +0300

    LTFU-search

--- app/src/main/java/com/example/mhealth/appointment_diary/defaulters_diary/losttofollow/LosttoFollowAdapter.java
+++ app/src/main/java/com/example/mhealth/appointment_diary/defaulters_diary/losttofollow/LosttoFollowAdapter.java
@@ -1581,8 +1581,7 @@ public class LosttoFollowAdapter extends BaseAdapter implements Filterable {
 
                 for(int i=0;i<filterList.size();i++){
 
-                    if( filterList.get(i).ccnumber.toUpperCase().contains(constraint) || filterList.get(i).phone.toUpperCase().contains(constraint) || filterList.get(i).fileserial.toUpperCase().contains(constraint
-                    )){
+                    if( filterList.get(i).ccnumber.toUpperCase().contains(constraint)){
 
 
                         LosttoFollowModel am=new LosttoFollowModel(filterList.get(i).getCcnumber(),filterList.get(i).getThename(),filterList.get(i).getPhone(),filterList.get(i).getApptype(),filterList.get(i).getDate(),filterList.get(i).getRead(),filterList.get(i).getPatientid(),filterList.get(i).getFileserial(),filterList.get(i).getInformantnumber(),filterList.get(i).getLastdateread(),filterList.get(i).getReadcount());
