f0531745e914451653fb57fe9b0c37b1aa102076
unknown <vikogutu77@gmail.com>
2023-05-04 13:17:32 +0300

    profile

--- app/src/main/java/com/mhealth/nishauri/ProfileActivity.java
+++ app/src/main/java/com/mhealth/nishauri/ProfileActivity.java
@@ -13,15 +13,19 @@ import android.widget.Toast;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.fxn.stash.Stash;
 import com.google.android.material.snackbar.Snackbar;
 import com.mhealth.nishauri.Activities.Auth.LoginActivity;
 import com.mhealth.nishauri.Activities.Auth.SignUpActivity;
 import com.mhealth.nishauri.Activities.MainActivity;
+import com.mhealth.nishauri.Models.UrlTable;
+import com.mhealth.nishauri.Models.User;
 import com.mhealth.nishauri.utils.Constants;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -33,11 +37,13 @@ public class ProfileActivity extends AppCompatActivity {
     String userExtra;
 
     String z;
+    private User loggedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        loggedInUser = (User) Stash.getObject(Constants.AUTH_TOKEN, User.class);
         init();
         btn_sub = findViewById(R.id.bt_prof);
         Bundle extras = getIntent().getExtras();
@@ -52,8 +58,16 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View view) {
                // Toast.makeText(ProfileActivity.this, "Profile created", Toast.LENGTH_SHORT).show();
 
+                if (ccc.getText().toString().isEmpty()){
+                    Toast.makeText(ProfileActivity.this, "Enter CCC Number", Toast.LENGTH_SHORT).show();
 
-                send(userExtra, ccc.getText().toString(), upi.getText().toString(), first.getText().toString());
+                }else if(first.getText().toString().isEmpty()){
+                    Toast.makeText(ProfileActivity.this, "Enter First Name", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+
+                send(userExtra, ccc.getText().toString(), upi.getText().toString(), first.getText().toString());}
 
             }
         });
@@ -73,6 +87,21 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void send(String userid, String cc, String up, String fname){
+        String auth_token = loggedInUser.getAuth_token();
+        String urls2 ="?user_id="+auth_token;
+
+        try{
+            List<UrlTable> _url =UrlTable.findWithQuery(UrlTable.class, "SELECT *from URL_TABLE ORDER BY id DESC LIMIT 1");
+            if (_url.size()==1){
+                for (int x=0; x<_url.size(); x++){
+                    z=_url.get(x).getBase_url1();
+                }
+            }
+
+        } catch(Exception e){
+
+        }
+
 
         JSONObject jsonObject = new JSONObject();
         try {
@@ -104,7 +133,7 @@ public class ProfileActivity extends AppCompatActivity {
                 .getAsJSONObject(new JSONObjectRequestListener(){
                     @Override
                     public void onResponse(JSONObject response) {
-//                        Log.e(TAG, response.toString());
+//                        Log.e(TAG, response.toString());+
 
                         //Toast.makeText(ProfileActivity.this, "Profile created", Toast.LENGTH_SHORT).show();
 
